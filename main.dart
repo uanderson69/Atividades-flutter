030fe7b3f7d38be70c810acb359691b8bdb3d8fb
uanderson69 <129782013+uanderson69@users.noreply.github.com>
2023-05-29 15:18:24 -0300

    Atividade 5

--- main.dart
+++ main.dart
@@ -1,84 +1,104 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(ComponenteInicial());
+  runApp(QuestionnaireApp());
 }
 
-class ComponenteInicial extends StatefulWidget {
+class QuestionnaireApp extends StatefulWidget {
   @override
-  _ComponenteInicialState createState() => _ComponenteInicialState();
+  _QuestionnaireAppState createState() => _QuestionnaireAppState();
 }
 
-class _ComponenteInicialState extends State<ComponenteInicial> {
-  String telaSelecionada = '';
+class _QuestionnaireAppState extends State<QuestionnaireApp> {
+  final List<Map<String, dynamic>> questions = [
+    {
+      'pergunta': 'Quantas capitais o Brasil tem?',
+      'alternativas': ['20', '27', '25', '30'],
+      'resposta': '',
+    },
+    {
+      'pergunta': 'O brasil é considerado o maior pais do mundo?',
+      'alternativas': ['Sim', 'Nao'],
+      'resposta': '',
+    },
+    {
+      'pergunta': 'Quantos planetas existem no sistema solar?',
+      'alternativas': ['7', '8', '9', '10'],
+      'resposta': '',
+    },
+  ];
 
+  int currentQuestionIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: 'Perguntas',
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Atividade avaliativa"),
+          title: Text('Perguntas'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  telaSelecionada = 'TELA AZUL';
-                });
-              },
-              child: Text("TELA AZUL"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  telaSelecionada = 'TELA VERDE';
-                });
-              },
-              child: Text("TELA VERDE"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  telaSelecionada = 'TELA PRETA';
-                });
-              },
-              child: Text("TELA PRETA"),
-            ),
-            if (telaSelecionada.isNotEmpty)
-              Expanded(
-                child: Container(
-                  color: _getCorTelaSelecionada(telaSelecionada),
-                  child: Center(
-                    child: Text(
-                      telaSelecionada,
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                questions[currentQuestionIndex]['pergunta']!,
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16.0),
+              Column(
+                children: List<Widget>.generate(
+                  questions[currentQuestionIndex]['alternativas'].length,
+                  (index) => RadioListTile(
+                    title: Text(questions[currentQuestionIndex]['alternativas'][index]),
+                    value: questions[currentQuestionIndex]['alternativas'][index],
+                    groupValue: questions[currentQuestionIndex]['resposta'],
+                    onChanged: (value) {
+                      setState(() {
+                        questions[currentQuestionIndex]['resposta'] = value;
+                      });
+                    },
                   ),
                 ),
               ),
-          ],
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    if (currentQuestionIndex < questions.length - 1) {
+                      currentQuestionIndex++;
+                    } else {
+                      // Questionário concluído
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Questionário concluído'),
+                            content: Text('O questionário foi respondido.'),
+                            actions: [
+                              TextButton(
+                                child: Text('OK'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  });
+                },
+                child: Text(
+                  currentQuestionIndex < questions.length - 1 ? 'Enviar' : 'Finalizar questionário',
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
-  }
-
-  Color _getCorTelaSelecionada(String tela) {
-    switch (tela) {
-      case 'TELA AZUL':
-        return Colors.blue;
-      case 'TELA VERDE':
-        return Colors.green;
-      case 'TELA PRETA':
-        return Colors.black;
-      default:
-        return Colors.transparent;
-    }
-  }
+      ),
+    );
+  }
 }
