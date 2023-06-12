3a1494b93d2e4bbf69b13fc54981bb4a9569749c
uanderson69 <129782013+uanderson69@users.noreply.github.com>
2023-06-12 15:05:26 -0300

    Atividade 1 2°B

---
+++ resposta.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+class Resposta extends StatelessWidget{
+  final String texto;
+  final void Function() quandoSelecionar;
+
+  Resposta(this.texto, this.quandoSelecionar);
+
+  Widget build(BuildContext context){
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(top: 10, bottom: 10),
+      child: 
+        ElevatedButton(
+            onPressed: quandoSelecionar, 
+            child: Text(this.texto)
+            ),
+    );
+  }
+}
